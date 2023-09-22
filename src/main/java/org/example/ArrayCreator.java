514879545ebb557d4f23f054b90f3e7487bef3ee
Ilya Makarov <ilyam2003@mail.ru>
2023-09-22 10:42:53 +0500

    ArrayCreator & SquareArray Created

--- src/main/java/org/example/ArrayCreator.java
+++ src/main/java/org/example/ArrayCreator.java
@@ -1,42 +1,74 @@
 package org.example;
 
+import org.example.MyExceptions.WrongArraySizeException;
+import org.example.MyExceptions.WrongColumnsNumberException;
+import org.example.MyExceptions.WrongElementException;
+import org.example.MyExceptions.WrongRowsNumberException;
+
 import java.util.Random;
 
 public class ArrayCreator {
     private static final Random rnd = new Random();
 
-    public String[][] makeArray() {
+    public SquareArray makeArray() {
         int columns = 4;
         int rows = 4;
-        if (rnd.nextInt(1) == 0) {
+        if (false/*rnd.nextInt(1) == 0*/) {
             columns += rnd.nextInt(3);
             rows += rnd.nextInt(3);
         }
-        return new String[rows][columns];
+        try {
+
+            return new SquareArray(rows, columns);
+        } catch (WrongArraySizeException e) {
+            System.out.println("Wrong array size detected");
+            e.printStackTrace();
+        }
+        return null;
     }
 
-    public void fillArray(String[][] stringArray) {
+    public void fillArray(SquareArray stringArray) {
         String myString;
         Character character;
-        for (int i = 0; i < stringArray.length; i++) {
-            for (int j = 0; j < stringArray[0].length; j++) {
+        for (int i = 0; i < stringArray.getRowsNumber(); i++) {
+            for (int j = 0; j < stringArray.getColumnsNumber(); j++) {
                 if (rnd.nextInt(5) == 0) {
-                    character = (char) (65+rnd.nextInt(57));
+                    character = (char) (65 + rnd.nextInt(57));
                     myString = String.valueOf(character);
                 } else {
                     myString = String.valueOf(rnd.nextInt(20));
                 }
-                stringArray[i][j] = myString;
+                stringArray.putElement(i, j, myString);
             }
         }
-//        return stringArray;
     }
-    public void showArray(String [][] stringArray){
+
+    /*public void showArray(String[][] stringArray) {
         for (int i = 0; i < stringArray.length; i++) {
             for (int j = 0; j < stringArray[0].length; j++) {
-                System.out.printf(" %s ",stringArray[i][j]);
+                System.out.printf(" %s ", stringArray[i][j]);
             }
             System.out.println();
         }
+    }*/
+
+
+    /*public int calculateSum(String[][] stringArray) throws WrongElementException {
+        int sum = 0;
+        for (int i = 0; i < stringArray.length; i++) {
+            for (int j = 0; j < stringArray[0].length; j++) {
+                try {
+                    sum += Integer.parseInt(stringArray[i][j]);
+                }catch (NumberFormatException e){
+                    throw new WrongElementException("Array must contain only numbers", stringArray[i][j]);
+                }
+            }
+        }
+        return sum;
+    }*/
+
+    public boolean isNumeric(String myString) {
+
+        return false;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,26 @@
 package org.example;
 
+import org.example.MyExceptions.WrongArraySizeException;
+import org.example.MyExceptions.WrongElementException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println((char) 65);
-        ArrayCreator ac = new  ArrayCreator();
-        String[][] myArray = ac.makeArray();
-        ac.fillArray(myArray);
-        ac.showArray(myArray);
+        ArrayCreator ac = new ArrayCreator();
+        SquareArray sa = ac.makeArray();
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            ac.fillArray(sa);
+            try {
+                sum = sa.calculateSum();
+                System.out.printf("The sum of elements is: %d\n",sum);
+                sum = 0;
+            } catch (WrongElementException e) {
+                System.out.println("Wrong element detected");
+                e.printStackTrace();
+            }
+            sa.showArray();
+            System.out.println();
+        }
+
     }
 }
